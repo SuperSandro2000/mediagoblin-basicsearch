e2984b381350a20e84865ee55945aa7990d36ea5
Sandro Jäckel <sandro.jaeckel@gmail.com>
2024-10-23 23:28:31 +0200

    Add packaging

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    version="0.1",
+    packages=["mediagoblin.plugins.basicsearch"],
+    include_package_data=True,
+    license="AGPLv3",
+)
